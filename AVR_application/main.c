f2ff36d5c010771286f1daa910955f8cbe279c72
Khaled Fathy <khaled.fathe16@gmail.com>
2022-02-02 02:26:17 +0200

    another

--- AVR_application/main.c
+++ AVR_application/main.c
@@ -1,6 +1,6 @@
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
-
+#include <util/delay.h>
 #include "DIO_interface.h"
 //#include "UART_interface.h"
 #include "SPI_interface.h"
@@ -10,8 +10,16 @@ int main()
 {
 
 MSPI_voidIntialize(M);
+while(1)
+{
+	MSPI_voidSendData(0b00001111);
+	_delay_ms(500);
+	MSPI_voidSendData(0b00000111);
+	_delay_ms(500);
+	MSPI_voidSendData(0b00000001);
+	_delay_ms(500);
+}
 
-MSPI_voidSendData(0xFF);
 
 return 0;
 }
