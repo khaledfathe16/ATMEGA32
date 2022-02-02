96e259c780edb56332df3030ebcf55c3cea7e1e7
Khaled Fathy <khaled.fathe16@gmail.com>
2022-02-03 00:51:22 +0200

    Last

--- AVR_application/main.c
+++ AVR_application/main.c
@@ -2,22 +2,21 @@
 #include "BIT_MATH.h"
 //#include <util/delay.h>
 #include "DIO_interface.h"
-#include "UART_interface.h"
+//#include "UART_interface.h"
 #include "SPI_interface.h"
 
 
 int main()
 {
-MUART_voidUartInitialization();
+//MUART_voidUartInitialization();
 MSPI_voidIntialize(M);
-u8 Data=0;
-while(1)
-{
-	Data = MUART_u8UartRecieveByte();
+u8 Data=0xFF;
+
+	//Data = MUART_u8UartRecieveByte();
 
 	MSPI_voidSendData(Data);
 
-}
+
 
 
 
