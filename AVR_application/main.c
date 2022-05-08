5383cd085e3a8479f0c3bc6ffa9e7b895ad833c3
Khaled Fathy <khaled.fathe16@gmail.com>
2022-05-08 22:28:09 +0200

    h

--- AVR_application/main.c
+++ AVR_application/main.c
@@ -1,58 +1,23 @@
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
-#include <util/delay.h>
+//#include <util/delay.h>
 #include "DIO_interface.h"
 //#include "UART_interface.h"
-#include "SPI_interface.h"
-#include "EEPROM_interface.h"
+//#include "SPI_interface.h"
+//#include "EEPROM_interface.h"
 
 int main()
 {
-u8 Local_u8represent;
-MDIO_voidSetPortMode(GPIOC,OUTPUT);
-MSPI_voidIntialize(M);
-_delay_ms(5);
-Local_u8represent = HEEPROM_u8ReadData(0x00);
 
-
-if(Local_u8represent==0xFF)
-{
-	u8 arr[10]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
-	HEEPROM_voidSend16Byte(0x00 ,arr);
-}
-
-
-
+	 MDIO_voidSetPortMode(GPIOD,OUTPUT);
+	 MDIO_voidSetPortValue(GPIOD,0xFF);
 /*
-HEEPROM_voidSendData(0x00,0xFF);
-HEEPROM_voidSendData(0x01,0xFF);
-HEEPROM_voidSendData(0x02,0xFF);
-HEEPROM_voidSendData(0x03,0xFF);
-HEEPROM_voidSendData(0x04,0xFF);
-HEEPROM_voidSendData(0x05,0xFF);
-HEEPROM_voidSendData(0x06,0xFF);
-HEEPROM_voidSendData(0x07,0xFF);
-HEEPROM_voidSendData(0x08,0xFF);
-HEEPROM_voidSendData(0x09,0xFF);
-*/
-
-
-
-while(1)
+    while(1)
 {
 
-	for(u8 i=0;i<=9;i++)
-	{
-
-		Local_u8represent = HEEPROM_u8ReadData(0x00+i);
-			MDIO_voidSetPortValue(GPIOC,Local_u8represent);
-			_delay_ms(100);
-
-
-	}
 
 }
-
+*/
 
 
 return 0;
