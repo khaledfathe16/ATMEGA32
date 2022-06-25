0a96cf70ef443f0b72f1a297ee1e50452d072be8
Khaled Fathy <khaled.fathe16@gmail.com>
2022-06-25 21:47:07 +0200

    ..

--- AVR_application/main.c
+++ AVR_application/main.c
@@ -1,10 +1,7 @@
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
-<<<<<<< HEAD
 
-=======
-//#include <util/delay.h>
->>>>>>> 5383cd085e3a8479f0c3bc6ffa9e7b895ad833c3
+
 #include "DIO_interface.h"
 //#include "UART_interface.h"
 //#include "SPI_interface.h"
@@ -12,34 +9,21 @@
 
 int main()
 {
-<<<<<<< HEAD
+
+	MDIO_voidSetPinMode(GPIOD,0,OUTPUT);
+
+
+	while(1)
+	{
+
+
+
+	}
 
 
 
 
 
-
-
-
-
-
-
-while(1)
-{
-
-
-=======
-
-	 MDIO_voidSetPortMode(GPIOD,OUTPUT);
-	 MDIO_voidSetPortValue(GPIOD,0xFF);
-/*
-    while(1)
-{
-
->>>>>>> 5383cd085e3a8479f0c3bc6ffa9e7b895ad833c3
-
-}
-*/
 
 
 return 0;
